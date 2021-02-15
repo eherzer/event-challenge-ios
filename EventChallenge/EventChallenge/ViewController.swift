6806719b5bd4c6493fa02f1869ca9a841bafd734
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-15 17:16:55 -0300

    Project creation

---
+++ EventChallenge/EventChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventChallenge
+//
+//  Created by Eduardo Herzer on 15/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
