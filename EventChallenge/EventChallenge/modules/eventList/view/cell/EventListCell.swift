a4445034c64b5ad133cb84d5b4e9db279ca55b96
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-16 20:59:43 -0300

    modules/eventList/view/cell: Create event list cell

---
+++ EventChallenge/EventChallenge/modules/eventList/view/cell/EventListCell.swift
@@ -0,0 +1,18 @@
+//
+//  EventListCell.swift
+//  EventChallenge
+//
+//  Created by Eduardo Herzer on 16/02/21.
+//
+
+import UIKit
+
+class EventListCell: UITableViewCell {
+    @IBOutlet weak var imgEvent: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbDate: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
