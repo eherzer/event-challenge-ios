281598c0d3e42b0b6878e6e473351c0ceeae9d96
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-16 20:58:53 -0300

    modules/eventList/service: Create event list service

---
+++ EventChallenge/EventChallenge/modules/eventList/service/EventListService.swift
@@ -0,0 +1,15 @@
+//
+//  EventListService.swift
+//  EventChallenge
+//
+//  Created by Eduardo Herzer on 16/02/21.
+//
+
+import Foundation
+
+class EventListService: EventListServiceProtocol {
+    func getEvents(success: @escaping ([Event]) -> Void, failure: @escaping (String) -> Void) {
+        //TODO
+        failure("Not implemented")
+    }
+}
