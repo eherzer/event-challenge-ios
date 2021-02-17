bc4152849be8b13b82ad9f1fd6cabfe7d2f222d0
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-16 21:38:07 -0300

    modules/eventList/service: Implement getEvents function

--- EventChallenge/EventChallenge/modules/eventList/service/EventListService.swift
+++ EventChallenge/EventChallenge/modules/eventList/service/EventListService.swift
@@ -7,9 +7,19 @@
 
 import Foundation
 
-class EventListService: EventListServiceProtocol {
+class EventListService: BaseService, EventListServiceProtocol {
     func getEvents(success: @escaping ([Event]) -> Void, failure: @escaping (String) -> Void) {
-        //TODO
-        failure("Not implemented")
+        let endpoint = "/events"
+        
+        request(endpoint: endpoint, method: .get) { (jsonResponse) in
+            if let jsonArray = jsonResponse,
+               let events = try? JSONDecoder().decode([Event].self, from: jsonArray.rawData()) {
+                success(events)
+            } else {
+                failure("Não recebeu a lista")
+            }
+        } failure: { (errorMessage) in
+            failure(errorMessage ?? "Não foi possível carregar a lista de eventos")
+        }
     }
 }
