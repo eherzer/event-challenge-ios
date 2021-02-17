864b4fa475939a11612fb0f6e34da49e24854fa5
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-16 23:14:47 -0300

    modules/eventDetails/protocols: Create event details protocols

---
+++ EventChallenge/EventChallenge/modules/eventDetails/protocols/EventDetailsPresenterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  EventDetailsPresenterProtocol.swift
+//  EventChallenge
+//
+//  Created by Eduardo Herzer on 16/02/21.
+//
+
+import Foundation
+
+protocol EventDetailsPresenterProtocol {
+    func loadEvent(id: String)
+}
