fc0fdd9305d334025b187b96268b14526cbbc747
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-02-17 00:00:51 -0300

    modules/eventDetails/view: Show event location on map

--- EventChallenge/EventChallenge/modules/eventDetails/view/EventDetailsVC.swift
+++ EventChallenge/EventChallenge/modules/eventDetails/view/EventDetailsVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Kingfisher
+import MapKit
 
 class EventDetailsVC: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
@@ -14,6 +15,7 @@ class EventDetailsVC: UIViewController {
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var lbDate: UILabel!
     @IBOutlet weak var lbDescription: UILabel!
+    @IBOutlet weak var mapImageView: UIImageView!
     
     private var idEvent: String?
     private var event: Event?
@@ -37,6 +39,8 @@ class EventDetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.mapImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(mapClicked)))
+        
         presenter = EventDetailsPresenter(view: self, service: EventDetailsService())
         
         if let event = event {
@@ -44,6 +48,53 @@ class EventDetailsVC: UIViewController {
         } else if let idEvent = idEvent {
             presenter.loadEvent(id: idEvent)
         }
+    }
+    
+    private func loadLocationMap() {
+        guard  let latitude = self.event?.latitude,
+               let longitude = self.event?.longitude else {
+            return
+        }
+        
+        let mapSnapshotOptions = MKMapSnapshotter.Options()
+        mapSnapshotOptions.mapType = .standard
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapSnapshotOptions.region = region
+        mapSnapshotOptions.size = mapImageView.frame.size
+        
+        let snapShotter = MKMapSnapshotter(options: mapSnapshotOptions)
+        snapShotter.start { (snapshot, error) in
+            if let snapShotImage = snapshot?.image, let coordinatePoint = snapshot?.point(for: location), let pinImage = UIImage(named: "map_marker_red_24dp") {
+                UIGraphicsBeginImageContextWithOptions(snapShotImage.size, true, snapShotImage.scale)
+                snapShotImage.draw(at: CGPoint.zero)
+                
+                let fixedPinPoint = CGPoint(x: coordinatePoint.x - pinImage.size.width / 2, y: coordinatePoint.y - pinImage.size.height)
+                pinImage.draw(at: fixedPinPoint)
+                let mapImage = UIGraphicsGetImageFromCurrentImageContext()
+                
+                self.mapImageView.image = mapImage
+                UIGraphicsEndImageContext()
+            }
+        }
+    }
+    
+    @objc private func mapClicked() {
+        guard  let latitude = self.event?.latitude,
+               let longitude = self.event?.longitude else {
+            return
+        }
+        
+        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+        let regionSpan = MKCoordinateRegion(center: coordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        let options = [
+            MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+            MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)
+        ]
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = self.event?.title
+        mapItem.openInMaps(launchOptions: options)
     }
 }
 
@@ -55,6 +106,8 @@ extension EventDetailsVC: EventDetailsViewProtocol {
         lbTitle.text = event.title
         lbDate.text = Date.from(seconds: event.date).humanFormat()
         lbDescription.text = event.description
+        
+        loadLocationMap()
         
         self.activityIndicator.stopAnimating()
     }
